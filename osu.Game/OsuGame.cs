cc1b3a4ddf867cb59e6679b7f997354327e71d7c
Derek Nguyen <dereknguyenbusiness@gmail.com>
2017-03-09 08:34:43 -0800

    Update OsuGame.cs

--- osu.Game/OsuGame.cs
+++ osu.Game/OsuGame.cs
@@ -1,6 +1,3 @@
-﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
-// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
-
 using System;
 using osu.Framework.Configuration;
 using osu.Framework.Screens;
